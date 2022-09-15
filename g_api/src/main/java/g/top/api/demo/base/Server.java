b02b400a70c54e933ee2625ec56c2dad02939d63
wanghaiguang <wb_wanghaiguang@meituan.com>
2022-09-15 15:51:37 +0800

    netty test

--- g_api/src/main/java/g/top/api/demo/base/Server.java
+++ g_api/src/main/java/g/top/api/demo/base/Server.java
@@ -27,20 +27,27 @@ public class Server {
 
         //设置连接组 和 工作组
         bootstrap.group(boss, worker)
+                //设置通道实现类型
                 .channel(NioServerSocketChannel.class)
+                //线程队列连接个数
                 .option(ChannelOption.SO_BACKLOG, 128)
+                //保持活动连接状态
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+                //通道初始化
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
+                        //设置通道处理器
                         ch.pipeline().addLast(new ServerHander());
                     }
                 });
 
         System.out.println("服务端 ready...");
 
+        //绑定启动端口
         ChannelFuture channelFuture = bootstrap.bind(9000).sync();
 
+        //对关闭通道监听
         channelFuture.channel().closeFuture().sync();
     }
 }
--- g_api/src/main/java/g/top/api/demo/base/ServerHander.java
+++ g_api/src/main/java/g/top/api/demo/base/ServerHander.java
@@ -8,10 +8,12 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
  * Created by wanghaiguang on 2022/9/15 上午11:21
+ * 自定义handler 必须继承netty规范
  */
 public class ServerHander extends ChannelInboundHandlerAdapter {
 
     @Override
+    //读取事件回调
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
 
@@ -23,11 +25,13 @@ public class ServerHander extends ChannelInboundHandlerAdapter {
     }
 
     @Override
+    //数据读取完毕回调
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.copiedBuffer("消息收到", Charsets.UTF_8));
     }
 
     @Override
+    //异常回调
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         ctx.close();
     }
